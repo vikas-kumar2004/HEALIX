effd126a0eba49f0607f8a14d80f0fafa65cc3ab
vikas kumar <vk8756008@gmail.com>
2026-01-29 18:48:57 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+// midleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+// heath check route
+app.get("/health", (req, res) => {
+  res.send("server is working");
+});
+// making our app ready deployment
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontent", "dist", "index.html"));
+  });
+}
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
